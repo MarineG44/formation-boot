a5ecd18065d49eb1bc9059bdc919f8c146cfb28f
MarineG44 <marine.guedon@live.fr>
2022-02-07 09:58:49 +0100

    ajout classes

--- formation-angular/src/app/model.ts
+++ formation-angular/src/app/model.ts
@@ -1,18 +1,65 @@
-export abstract class Personne {
+export abstract class Utilisateur {
 	id: number;
 	version: number;
-	civilite: string;
 	nom: string;
 	prenom: string;
-	adresse : Adresse;
+    email: string;
+    droit: string;
+    telephone: string;
+    identifiant: string;
+    motDePasse: string;
+	adr: Adresse;
+    filieres: Array<Filiere> = new Array<Filiere>();
 	
-	constructor(id?: number, version?:number, civilite?: string, nom?: string, prenom?: string, adresse?: Adresse) {
+	constructor(id?: number, version?:number, nom?: string, prenom?: string, email?: string, droit?: string, telephone?: string, identifiant?: string,
+        motDePasse?: string, adr?: Adresse) {
 		this.id = id;
 		this.version = version;
-		this.civilite = civilite;
 		this.nom = nom;
-		this.prenom = prenom;
-		this.adresse = adresse;
+        this.prenom = prenom;
+		this.email = email;
+        this.droit = droit;
+        this.telephone = telephone;
+        this.identifiant = identifiant;
+        this.motDePasse = motDePasse;
+		this.adr = adr;
 	}
 	
+}
+
+export class Stagiaire extends Utilisateur {
+    dateNaissance: string;
+    cursus: Array<Cursus> = new Array<Cursus>();
+
+    constructor(id?: number, version?:number, nom?: string, prenom?: string, email?: string, droit?: Droit, telephone?: string, identifiant?: string,
+        motDePasse?: string, adr?: Adresse) {
+            super(id, version, nom, prenom, email, droit, telephone, identifiant,
+                motDePasse, adr)
+            this.dateNaissance;
+        }
+
+}
+
+export class Cursus {
+    id: number;
+    version: number;
+    stagiaire: Stagiaire;
+    filiere: Filiere;
+    noteC: number;
+    commentairesC: string;
+    noteT: number;
+    commentairesT: string;
+
+    contructor(id?: number, version?: number, stagiaire?: Stagiaire, filiere?: Filiere, noteC?: number, commentairesC?: string, noteT?: number, commentairesT?: string) {
+    this.id = id;
+    this.version = version;
+    this.stagiaire = stagiaire;
+    this.filiere = filiere;
+    this.noteC = noteC;
+    this.commentairesC = commentairesC;
+    this.noteT = noteT;
+    this.commentairesT = commentairesT;
+
+    }
+
 }
