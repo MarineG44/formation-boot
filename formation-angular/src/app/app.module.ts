cda0a8bf15b788af1857ad77b1d57bdc687d6060
MarineG44 <marine.guedon@live.fr>
2022-02-07 10:32:49 +0100

    ajouts

--- formation-angular/src/app/app.module.ts
+++ formation-angular/src/app/app.module.ts
@@ -2,15 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StagiaireComponent } from './stagiaire/stagiaire.component';
+import { CursusComponent } from './cursus/cursus.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const routes: Routes = [
+  {path:"accueil", component: AccueilComponent},
+  {path:"stagiaire", component: StagiaireComponent},
+  {path:"cursus", component: CursusComponent}
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StagiaireComponent,
+    CursusComponent,
+    AccueilComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+
 export class AppModule { }
